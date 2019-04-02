6e538ed19b7ac99a833e443f161b51ab46ddb60e
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-01 22:51:08 -0500

    Initial Commit

---
+++ vision-app-dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vision-app-dev
+//
+//  Created by Ricardo Herrera Petit on 4/1/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
